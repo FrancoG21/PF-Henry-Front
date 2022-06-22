8bc80caecee87a0cb4d681080bf5fcf3522e36a1
ynavarro44 <yinanavarrov@gmail.com>
2022-06-22 14:19:00 -0500

    Agregando carpetas a components

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,33 @@
+import {
+    // aqui nombre de action
+    GET_PETS
+} from '../actions/nameAction'
+
+const initialState = {
+    // aqui mis estados
+    pets: [],
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case GET_PETS: {
+            return {
+                ...state,
+                pets: action.payload
+            }
+        }
+
+        default: {
+            return state
+        }
+
+
+    }
+}
+
+
+
+
+
+export default rootReducer
