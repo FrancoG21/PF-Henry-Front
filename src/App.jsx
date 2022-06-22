04db9f6b493c59cea86c0e932368db2de6bb11ca
efam2907 <efam2907@gmail.com>
2022-06-22 15:08:59 -0500

    Creacion de la searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>PF Henry</h1>
+      
     </div>
   )
 }
--- src/redux/actions/nameAction.js
+++ src/redux/actions/nameAction.js
@@ -1,1 +1,2 @@
 export const GET_PETS = 'GET_PETS';
+export const GET_PET_NAME = 'GET_PET_NAME';
