e3dc185b2d095460d9ee451d97729b35d63aa28a
ynavarro44 <yinanavarro44>
2022-06-23 10:00:10 -0500

    About y Card

---
+++ src/components/Users/Card/Card.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styles from "./Cards.css";
+
+// export default function PetCard({ id, name, image }) {
+//     return (
+//         <div>
+//             <Link to={`/mascotas/${id}`}><img src={image} alt={name} /></Link>
+//             <h3>{name}</h3>
+//         </div>
+
+//     )
+// }
