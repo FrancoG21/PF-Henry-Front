e3a3bc0399a5f524ef2c96601d5c1a4c7fc8754d
ynavarro44 <yinanavarro44>
2022-06-25 17:38:04 -0500

    Carrusel y form para perdidos

--- src/pages/To Adopt/ToAdopt.jsx
+++ src/pages/To Adopt/ToAdopt.jsx
@@ -1,12 +1,12 @@
-import React ,{useEffect, useState} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from "react-router-dom";
-import {getPets} from '../../redux/actions/index'
+import { getPets } from '../../redux/actions/index'
 import Card from '../../components/Users/Card/Card'
 import { Grid, Container, TitleAdopt, ButtonCreate, ButtonLink } from './StyledToAdopt';
 import Paginate from '../../components/Users/Paginate/Paginate';
 import Searchbar from '../../components/Users/Searchbar/Searchbar'
-
+// import Slideshow from '../../components/Users/Lost/Slideshow/Slideshow'
 
 export default function ToAdopt() {
 
@@ -15,43 +15,50 @@ export default function ToAdopt() {
     const petsAmount = useSelector(state => state.petsAmount)
     const errors = useSelector(state => state.errors)
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getPets())
         console.log(pets.pets)
-    },[dispatch])
+    }, [dispatch])
 
-   const paginateFunction = (page)=>{
-    dispatch(getPets(page))
-   }
+    const paginateFunction = (page) => {
+        dispatch(getPets(page))
+    }
 
     return (
         <div>
             <TitleAdopt>To Adopt</TitleAdopt>
-            <Searchbar/>
+            <Searchbar />
 
             <ButtonLink to={'/petcreate'}>
                 <ButtonCreate>Cargar un pet</ButtonCreate>
             </ButtonLink>
+            {/* <ButtonLink to={'/petcreate'}>
+                <ButtonCreate>Lost</ButtonCreate>
+            </ButtonLink> */}
 
             <Container>
                 <Grid>
 
-                    { errors[0] === 'mensaje' ? <p>no existe el pet</p> : 
-                    
-                    !pets ? <p>Loading</p> : pets?.map(p =>{
-                        return <Card
-                            key = {p.id}
-                            id = {p.id}
-                            name = {p.name}
-                            image = {p.image}
-                        />
-                    })} 
-                </Grid> 
+                    {errors[0] === 'mensaje' ? <p>no existe el pet</p> :
+
+                        !pets ? <p>Loading</p> : pets?.map(p => {
+                            return <Card
+                                key={p.id}
+                                id={p.id}
+                                name={p.name}
+                                image={p.image}
+                            />
+                        })}
+                </Grid>
+                {/* <main>
+                    <p>Lost Pets</p>
+                    <Slideshow />
+                </main> */}
             </Container>
-            <Paginate 
+            <Paginate
                 total={petsAmount}
                 petsPerPage={6}
-                paginateFunction = {paginateFunction} />
+                paginateFunction={paginateFunction} />
         </div>
     )
 }
