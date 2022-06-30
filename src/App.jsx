ae4be98666e86eaa5647d83982b04cf0c5fe1e85
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-30 20:38:47 -0300

    donation base

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Donation from "./pages/Donation/Donation";
 import UserAdoptPetForm from "./components/Users/UserAdoptPetForm/UserAdoptPetForm"
 import UserTransitPetForm from "./components/Users/UserTransitPetForm/UserTransitPetForm"
 import UserProfile from "./pages/UserProfile/UserProfile";
+import Success from "./pages/Donation/results/Success";
+import Failure from "./pages/Donation/results/Failure";
 
 //  MODO OSCURO //
 import { ThemeProvider } from "styled-components";
@@ -35,6 +37,8 @@ function App() {
             <Route path="/petcreate" element={<PetCreate />}></Route>
             <Route path="/lostform" element={<LostForm />}></Route>
             <Route path="/donation" element={<Donation />}></Route>
+            <Route path="/donation/success" element={<Success />}></Route>
+            <Route path="/donation/failure" element={<Failure />}></Route>
             <Route path="/useradoptpet" element={<UserAdoptPetForm />}></Route>
             <Route path="/usertransitpet" element={<UserTransitPetForm />}></Route>
             <Route path="/userprofile" element={<UserProfile />}></Route>
--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,5 +1,33 @@
+import { Form, Formik, Field } from "formik";
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
 
 export default function Donation() {
-  return <div>Componente Donation</div>;
+  const [url, setUrl] = useState("");
+
+  return (
+    <>
+    <Formik
+      initialValues={{ amount: 200}}
+      onSubmit={(values) => {
+        axios.post('/payment', {
+          unit_price: Number(values.amount),
+          failure: 'http://localhost:3000/donation/failure',
+          success: 'http://localhost:3000/donation/success'
+        }).then(r=>setUrl(r.data.url))
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="amount" type="text"/>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+      {
+        url && window.location.replace(url)
+      }
+    </>
+  );
 }
