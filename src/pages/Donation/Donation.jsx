124bfc4a4c14300d34906bdf651b9409f8c5742b
ynavarro44 <yinanavarro44>
2022-07-01 08:23:50 -0500

    formulario con css

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -2,6 +2,7 @@ import { Form, Formik, Field } from "formik";
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
+import Classes from './Donation.module.css';
 
 
 export default function Donation() {
@@ -9,22 +10,25 @@ export default function Donation() {
 
   return (
     <>
-    <Formik
-      initialValues={{ amount: 200}}
-      onSubmit={(values) => {
-        axios.post('/payment', {
-          unit_price: Number(values.amount),
-          failure: 'http://localhost:3000/donation/failure',
-          success: 'http://localhost:3000/donation/success'
-        }).then(r=>setUrl(r.data.url))
-      }}
-    >
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="amount" type="text"/>
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      <Formik
+        initialValues={{ amount: 200 }}
+        onSubmit={(values) => {
+          axios.post('/payment', {
+            unit_price: Number(values.amount),
+            failure: 'http://localhost:3000/donation/failure',
+            success: 'http://localhost:3000/donation/success'
+          }).then(r => setUrl(r.data.url))
+        }}
+      >
+        <Form>
+          <div className={Classes.container}>
+            <label htmlFor="dona">AQUI PUEDES DONAR!</label>
+            <label htmlFor="cantidad">Valor a donar</label>
+            <Field name="amount" type="text" />
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      </Formik>
       {
         url && window.location.replace(url)
       }
