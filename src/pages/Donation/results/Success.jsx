e1535198bf580db73591e7a2a8b799fb93a2bce6
ynavarro44 <yinanavarro44>
2022-07-02 14:54:48 -0500

    success

--- src/pages/Donation/results/Success.jsx
+++ src/pages/Donation/results/Success.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./success.module.css";
+import dogs from "../../img/dogs1.gif";
+
 
 export default function Success() {
-    return <div>Success</div>
+    return (
+        <>
+            <div className="Success">
+
+                <h2> TU DONACIÓN ES MUY VALIOSA PARA NOSOTROS!</h2>
+                <span>Recibirás un correo confirmando tu donación</span>
+                <h3>Adoptar, es la mejor opción!</h3>
+                <img src={dogs} alt="dog" />
+
+            </div>
+        </>
+    );
 }
