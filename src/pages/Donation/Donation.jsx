0349be5be3604c33333ad7495a21d0b7d8c6e5be
Franco <fgimenez029@gmail.com>
2022-07-07 11:35:00 -0300

    fix css donation

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -3,7 +3,26 @@ import axios from "axios";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import "./Donation.module.css";
-import { BackgroundDonation, ButtonDonation, ButtonSuscription, ContainerButton, SubTitle, Suscription, TitleDonation } from "./styledDonation";
+import { BackgroundDonation,
+  ButtonDonation, 
+  ButtonSuscription, 
+  ContainerButton, 
+  ContainerCard,
+  ImagePet, 
+  Post,
+  HeaderPost,
+  SubTitle,
+  TitleDonation, 
+  BodyContent,
+  PostContent,
+  TitleCard,
+  TextCard,
+  ContainerTextDonation,
+  Container,
+  SubtitleDonation,
+  TextDonation,
+  ContainerButtonSuscription,
+} from "./styledDonation";
 
 export default function Donation() {
   const [url, setUrl] = useState("");
@@ -33,19 +52,96 @@ export default function Donation() {
   return (
     <>
       <BackgroundDonation>
-        <TitleDonation>DONACIÓN:</TitleDonation>
+        <TitleDonation>Ayudanos con tu Donación</TitleDonation>
         <SubTitle>Nuestro trabajo depende enteramente de tu ayuda. Necesitamos tu apoyo para solventar el enorme costo de manutención de muchos animales ya que no poseemos ayuda del estado ni de empresas privadas. Gracias a tu aporte logramos seguir con nuestras tareas, para lograr un futuro libre de maltrato y abandono animal. Ayudanos a seguir ayudándolos con el monto que puedas y quieras, no hay montos estipulados ni tiempo de permanencia.
           Gracias por permitirnos seguir trabajando por ellos.
         </SubTitle>
+
+        <div>
+          <SubtitleDonation>¿Que recibiran los animales con tu donación?</SubtitleDonation>
+        </div>
+
+        <ContainerCard>
+          <Post>
+            <HeaderPost>
+              <ImagePet src='https://img.freepik.com/foto-gratis/perro-callejero-hambriento-comiendo-tazon-centro-adopcion_87555-2460.jpg' alt='comida'/>
+            </HeaderPost>
+            <BodyContent>
+              <PostContent>
+                <TitleCard>Alimento</TitleCard>
+                <TextCard>
+                  Alimento balanceado para perros y gatos, tanto como adulto como de cachorro, pouchs de alimento húmedo (Pedigree), golosinas para animales, etc.
+                </TextCard>
+              </PostContent>
+            </BodyContent>
+          </Post>
+
+          <Post>
+            <HeaderPost>
+              <ImagePet src='https://www.uchile.cl/.imaging/default/dam/imagenes/Uchile/imagenes-noticias/169350_1_dia-del-veterinario-04-l_L/jcr:content.jpg' alt='salud'/>
+            </HeaderPost>
+
+            <BodyContent>
+              <PostContent>
+                <TitleCard>Salud</TitleCard>
+                <TextCard>
+                  Antiparasitos internos, pipetas antipulgas, toltrazol en comprimidos, algen, spectryl, antiartrósicos / regeneradores osteoarticulares/ condoprotectores (Artrin/ Artroglican/ Gerioox / Sosten CG / Osteocart Plus), Vitamina E 200, Polferon, Contal, Rifocina, etc.
+                </TextCard>
+              </PostContent>
+            </BodyContent>
+          </Post>
+
+          <Post>
+            <HeaderPost>
+              <ImagePet src='https://www.conclusion.com.ar/wp-content/uploads/2019/05/Perros.jpg' alt='cama'/>
+            </HeaderPost>
+
+            <BodyContent>
+              <PostContent>
+                <TitleCard>Kits</TitleCard>
+                <TextCard>
+                  Correas y collares, cuchas, camas, mantas, jaulas de exposición, platos para la comida y bebida, transportadoras, elementos de limpieza (lavandina, ambitus, trapos), etc.
+                </TextCard>
+              </PostContent>
+            </BodyContent>
+          </Post>
+        </ContainerCard>
+
+        <ContainerTextDonation>
+          <Container>
+            <SubtitleDonation>Tu Donación nos ayuda a ayudar</SubtitleDonation>
+            <TextDonation>
+              Te agradecemos desde ya tu voluntad de colaborar con nuestra misión de encontrar hogares definitivos para nuestros rescatados, y también brindar un hogar digno a los que no tienen la suerte de ser adoptados.  
+            </TextDonation>
+          </Container>
+
+          <Container>
+            <SubtitleDonation>Cada Centavo suma</SubtitleDonation>
+            <TextDonation>
+              Y nos ayuda a garantizar que nuestros rescatados reciban todo lo que necesitan hasta que una familia los elija para siempre.  
+            </TextDonation>
+          </Container>
+        </ContainerTextDonation>
+
+
+        <div>
+          <SubtitleDonation>Doná con Mercado Pago</SubtitleDonation>
+            <TextDonation>Un importe fijo con Mercado Pago
+              (Todas las tarjetas de crédito y débito, Pago Fácil, Rapipago, Link, Banelco o desde la APP)
+            </TextDonation>
+        </div>
+
         <ContainerButton>
           {
-            precios.map(value => { return <ButtonDonation onClick={(e) => realizarPagoUnico(e)} value={value}>{value} </ButtonDonation> })
+            precios.map(value => { return <ButtonDonation onClick={(e) => realizarPagoUnico(e)} value={value}>Doná {value}$ </ButtonDonation> })
           }
         </ContainerButton>
 
-
-        <Suscription>SUSCRIPCIÓN</Suscription>
-        <ButtonSuscription onClick={(e) => realizarPagoSub(e)} value={200}>{200}</ButtonSuscription>
+        <SubtitleDonation>SUSCRIPCIÓN</SubtitleDonation>
+        <TextDonation>Con nuestro sistema de suscripción de 1500$ ARS y con $50 por día colaboras para poder darle alimentos y proveer de las vacunas básicas a los recién rescatados</TextDonation>
+        <ContainerButtonSuscription>
+          <ButtonSuscription onClick={(e) => realizarPagoSub(e)} value={200}>Doná {200}$</ButtonSuscription>
+        </ContainerButtonSuscription>
       </BackgroundDonation>
     </>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,7 +6,7 @@ import { loginManual } from '../../redux/actions/index';
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import  GoogleLogin  from 'react-google-login'
+// import  GoogleLogin  from 'react-google-login'
 // import s from './login.module.css'
 import Swal from "sweetalert2";
 import axios from 'axios'
@@ -141,12 +141,12 @@ export default function Login() {
 //     window.location.replace(url + '/auth/google/callback', '_self');
 //   }
   
-function responseGoogle(response) {
-  console.log('responseGoogle', response)
-}
-function alert(result) {
- console.log(result)
-}
+// function responseGoogle(response) {
+//   console.log('responseGoogle', response)
+// }
+// function alert(result) {
+//  console.log(result)
+// }
 
 
   return (
@@ -157,13 +157,13 @@ function alert(result) {
           {errors.email && <Errors>{errors.email}</Errors>}
           
 
-          <GoogleLogin
+          {/* <GoogleLogin
     clientId="6229358800-jmcgp4kol677o5qvhs02hnkaclvk1174.apps.googleusercontent.com"
     buttonText="Login"
     onSuccess={responseGoogle}
     onFailure={alert}
     cookiePolicy={'single_host_origin'}
-  />
+   /> */}
 
 
 
