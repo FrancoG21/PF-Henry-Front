611d0f916bacf1cb1eebb5fb1e2b75a6c3c79c41
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-07-12 23:13:52 -0300

    redirect whithout user working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Pets from "./pages/Pets/Pets";
 import NavBar from "./components/Users/NavBar/NavBar";
@@ -44,13 +44,11 @@ import DonationCard from "./pages/UserProfile/DonationCard/DonationCard";
 function App() {
   const dispatch = useDispatch();
 
-  // const userInfo = useSelector((state) => state.usuario)
+  const token = useSelector((state) => state.usuario)
 
   useEffect(() => {
     window.localStorage.getItem('userInfo') && dispatch(upLogin(JSON.parse(localStorage.getItem("userInfo"))))
   }, [])
-
-  // console.log(JSON.parse(localStorage.getItem("userInfo")))
 
   const [theme, setTheme] = useDarkMode();
   const themeMode = theme === "light" ? "light" : "dark";
@@ -65,32 +63,36 @@ function App() {
             <Route path="/adopt" element={<Pets />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/petdetail/:id" element={<PetDetail />}></Route>
-            <Route path="/petcreate" element={<PetCreate />}></Route>
+            <Route path="/petcreate" element={token ? <PetCreate /> : <Navigate to='/' replace/>}></Route>
             {/* <Route path="/lostform" element={<LostPets />}></Route> */}
-            <Route path="/donation" element={<Donation />}></Route>
+            <Route path="/donation" element={token ? <Donation /> : <Navigate to='/' replace/>}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/register" element={<Register />}></Route>
-            <Route path="/useradoptpet/:id" element={<UserAdoptPetForm />}></Route>
-            <Route path="/usertransitpet/:id" element={<UserTransitPetForm />}></Route>
-            <Route path="/donation/success" element={<Success />}></Route>
-            <Route path="/donation/failure" element={<Failure />}></Route>
+            <Route path="/useradoptpet/:id" element={token ? <UserAdoptPetForm /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/usertransitpet/:id" element={token ? <UserTransitPetForm /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/donation/success" element={token ? <Success /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/donation/failure" element={token ? <Failure /> : <Navigate to='/' replace/>}></Route>
             {/* <Route path="/useradoptpet" element={<UserAdoptPetForm />}></Route>
             <Route path="/usertransitpet" element={<UserTransitPetForm />}></Route> */}
-            <Route path="/userprofile" element={<UserProfile />}></Route>
-            <Route path="/useritsmypet/:id" element={<UserItsMyPetForm />}></Route>     
+            <Route path="/userprofile" element={token ? <UserProfile /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/useritsmypet/:id" element={token ? <UserItsMyPetForm /> : <Navigate to='/' replace/>}></Route>     
             <Route path="/lostpets" element={<LostPets />}></Route>      
-            <Route path="/admin" element={<AdminHome />}></Route>
-            <Route path="/admin/pets" element={<AdminPets />}></Route>
-            <Route path="/admin/users" element={<AdminUsers />}></Route>
-            <Route path="/admin/donation" element={<AdminDonation />}></Route>
-            <Route path="/admin/formadopt" element={<FormAdopt />}></Route>
-            <Route path="/admin/formlost" element={<FormLostAnimals />}></Route>
-            <Route path="/admin/formtransit" element={<FormTransit />}></Route>
-            <Route path="/admin/profilepets/:id" element={<ProfilePets />}></Route>
-            <Route path="/admin/petitionuser/:id" element={<Peticiones />}></Route>
+            <Route path="/admin" element={token ? <AdminHome /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/pets" element={token ? <AdminPets /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/users" element={token ? <AdminUsers /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/donation" element={token ? <AdminDonation /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/formadopt" element={token ? <FormAdopt /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/formlost" element={token ? <FormLostAnimals /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/formtransit" element={token ? <FormTransit /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/profilepets/:id" element={token ? <ProfilePets /> : <Navigate to='/' replace/>}></Route>
+            <Route path="/admin/petitionuser/:id" element={token ? <Peticiones /> : <Navigate to='/' replace/>}></Route>
             {/* <Route path="/useritsmypet/:id" element={<UserItsMyPetForm />}></Route> */}
             {/* <Route path="/lostpets" element={<LostPets />}></Route> */}
-            <Route path="/chatbot" element={<ContenidoChatb />}></Route>                     
+            <Route path="/chatbot" element={<ContenidoChatb />}></Route>
+            <Route
+                path="*"
+                element={<Navigate to="/" replace />}
+            />                     
           </Routes>         
           <Footer />
         </ThemeProvider>
