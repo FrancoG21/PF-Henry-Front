dff66bfe79c8ca06d6a7455b0c1d008d94ece72c
lucas1995giorgi@gmail.com <lucas1995giorgi@gmail.com>
2022-07-14 00:58:39 -0300

    nervios

--- src/components/Admin/AdminUsers/StyledAdminUsers.js
+++ src/components/Admin/AdminUsers/StyledAdminUsers.js
@@ -6,7 +6,7 @@ import Card from '@mui/material/Card';
 
 export const BackgroundListUsers = styled.div`
     background-color: ${(props) => props.theme.background};
-    height: 150vh;
+    height: auto;
 `
 
 export const BackgroundPetition = styled.div`
